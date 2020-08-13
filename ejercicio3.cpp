f3a6cb0e534eac4f87e49f7ceedca2235f715d97
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-08-13 18:55:41 -0500

    Modified ejercicio 3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -11,11 +11,25 @@ int sum(int a[][2]){
     return total;
 }
 
+int sum2(int a[][2], int n){
+    //Base case
+    if (n == 0){
+        return n;
+    }
+    int total = 0;
+    for (int i = 0; i<n/2; i++){
+        for(int j = 0; j<n/2; j++){
+            total += a[i][j];
+        }
+    }
+    return total;
+}
+
 int main(){
     //Testing
     int a[2][2] = {{1,2},{3,4}};
     int ans = 0;
-    ans = sum(a);
+    ans = sum2(a,4);
     std::cout << ans << std::endl;
 
     return 0;
