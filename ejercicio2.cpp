67d769ed6be4c333e0575d7e3c3eedc0e467bcaf
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-08-13 15:19:50 -0500

    Modified ejercicio 2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,14 +4,14 @@
 
 int sum(std::vector <int> v, int n){
     //Base case
-    if (v.size() == 0)
+    if (n == 0)
         return 0;
     return (sum(v, n-1) + v.at(n-1));
 }
 
 int main(){
-    std::vector <int> v {1,2,3,4,5};
-    int n = 5;
+    std::vector <int> v {2,3};
+    int n = 2;
     int ans = sum(v, n);
     std::cout << ans << std::endl;
 
