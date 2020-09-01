8d349c393a436a6829ea8c2d454fbea1cf317a55
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-08-31 20:10:50 -0500

    Added search algorithms

--- algoritmosBusqueda/main.cpp
+++ algoritmosBusqueda/main.cpp
@@ -1,13 +1,13 @@
 //Developed by Alfredo Jeong Hyun Park (A01658259)
 //Copyright © 2020. All rights reserved.
 #include "Search.h"
-#include "Search.cpp"
 #include <vector>
 #include <iostream>
 
+template <typename T>
 int main(){
-    std::vector <int> v = {1,2,3,4};
-    Search s=v;
+    std::vector <T> v = {1,2,3,4};
+    Search s(v);
     std::cout << s.sequentialSearch(2);
 
     return 0;
