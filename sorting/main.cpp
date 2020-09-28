b10a77286fb4f9ec444991ff8759320c733dcd33
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-09-28 13:11:21 -0500

    Update

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -11,24 +11,49 @@ int main(){
     //Creating random vector
     srand(time(NULL));
     std::vector <int> v;
-    for (int i = 0; i<10; i++){
-        v.push_back(rand()%100);
+    for (int i = 0; i<100000; i++){
+        v.push_back(rand()%100000);
     }
     
+    //Selection Sort
+    DECLARE_TIMING(s);
+    START_TIMING(s);
     SelectionSort <int> s;
     s.sort(v);
+    STOP_TIMING(s);
+    SHOW_TIMING(s, "Selection Sort: ");
     
+    //Bubble Sort
+    DECLARE_TIMING(s1);
+    START_TIMING(s1);
     BubbleSort <int> b;
     b.sort(v);
-    
+    STOP_TIMING(s1);
+    SHOW_TIMING(s1, "Bubble Sort: ");
+
+    //Insertion Sort
+    DECLARE_TIMING(s2);
+    START_TIMING(s2);
     InsertionSort<int> i;
     i.sort(v);
+    STOP_TIMING(s2);
+    SHOW_TIMING(s2, "Insertion Sort: ");
 
+    //Merge Sort
+    DECLARE_TIMING(s3);
+    START_TIMING(s3);
     MergeSort <int> m;
     m.sort(v, 0, 9);
+    STOP_TIMING(s3);
+    SHOW_TIMING(s3, "Merge Sort: ");
 
+    //Quick Sort
+    DECLARE_TIMING(s4);
+    START_TIMING(s4);
     QuickSort<int> q;
     q.sort(v, 0, 9);
+    STOP_TIMING(s4);
+    SHOW_TIMING(s4, "Quick Sort: ");
 
     return 0;
 }
