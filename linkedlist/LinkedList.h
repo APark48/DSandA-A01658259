81751316ddecd2837c9d8aaa64218f15a5a671a0
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-10-01 12:32:03 -0500

    Added linkedlist folder with files

--- linkedlist/LinkedList.h
+++ linkedlist/LinkedList.h
@@ -1,26 +1,2 @@
 //Developed by Alfredo Jeong Hyun Park (A01658259)
 //Copyright © 2020. All rights reserved.
-#include "Node.h"
-
-template <typename T>
-class LinkedList{
-private:
-    Node<T>* head;
-public:
-    //Constructor and destructor
-    LinkedList(){
-        head = nullptr;
-    }
-    LinkedList(Node<T>* head){
-        this->head=head;
-    }
-    ~LinkedList();
-
-    //Functions
-    void print();
-    int count(T target);
-    void deleteList();
-    void sortedInsert(Node<T>* newNode);
-    void removeDuplicates();
-    void reverse();
-};
--- linkedlist/Node.h
+++ linkedlist/Node.h
@@ -1,19 +1,2 @@
 //Developed by Alfredo Jeong Hyun Park (A01658259)
 //Copyright © 2020. All rights reserved.
-
-template <typename T>
-class Node{
-private:
-    T value;
-    Node* next;
-public:
-    Node(){
-        T value = NULL;
-        next = nullptr;
-    }
-    Node(T value, Node *next){
-        this->value=value;
-        this->next=next;
-    }
-    ~Node();
-};
