b92b90426ba0ef25b6aad2719cb9e8465034e9aa
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-10-01 10:39:52 -0500

    Updated main.cpp

---
+++ linkedlist/main.cpp
@@ -0,0 +1,10 @@
+//Developed by Alfredo Jeong Hyun Park (A01658259)
+//Copyright © 2020. All rights reserved.
+#include "LinkedList.h"
+#include "Node.h"
+#include <iostream>
+
+int main(){
+
+    return 0;
+}
